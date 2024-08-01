5afb186b6c5d63466d18c1814a08d0176fdfd9d5
mohammed ali almaliky <32299659+mhmdali98@users.noreply.github.com>
2024-08-01 05:11:40 +0300

    Update detail-movie.vue

--- pages/detail-movie.vue
+++ pages/detail-movie.vue
@@ -10,7 +10,7 @@
           <div class="text">
             <h4 class="use-text-title2">
               <span class="use-text-subtitle2">Vivamus sit amet</span>
-              The Lorem Ipsum Dolor Sit 
+              The Lorem Ipsum Dolor Sit Amet
             </h4>
             <div class="property">
               <div class="star">
